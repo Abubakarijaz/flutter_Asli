2507ca63f5c42ce26c19f4b8457d7a0b557c2a0b
Abubakar ijaz <abubakarijaz23@gmail.com>
2023-06-16 22:14:27 +0500

    orignal appp

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/quiz.dart';
+
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Home(),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
